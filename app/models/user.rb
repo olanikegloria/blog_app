4293b652c32328d335e5c1e1df66b156d999b2f9
olanike olatunji <olanikegloria2020@gmail.com>
2023-09-20 11:13:08 +0100

    created asscoiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
-end
+    has_many :posts, foreign_key: 'author_id', dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
+  end
+  
