c8e889f88a54fffe9325097847984dff615c0a4e
olanike olatunji <olanikegloria2020@gmail.com>
2023-09-21 07:05:23 +0100

    created models and association

--- db/migrate/20230920032050_create_comments.rb
+++ db/migrate/20230920032050_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :user, foriegn_key: true
-      t.references :post, foriegn_key: true
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
       t.string :text
       
       t.timestamps
