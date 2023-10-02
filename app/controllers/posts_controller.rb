da551fc62eaa51ec4a9976d2686693d46d7b7660
M.Nisar Ali <dr.nisaralig@gmail.com>
2023-10-02 20:51:17 +0500

    Fix n+1 issues in posts_controller's show action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(:author, comments: :user).find(params[:id])
     @current_user = User.first
     @like = Like.new
   end
