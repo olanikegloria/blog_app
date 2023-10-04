f31000a785fc0737c612e74c23f2086d7726cac6
olanike olatunji <olanikegloria2020@gmail.com>
2023-10-04 15:25:07 +0100

    Add cancancan to the gemfile and created ability file

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   end
   root "users#index"
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show, :create, :new] do
-      resources :comments, only: [:new, :create]
+    resources :posts, only: [:index, :show, :create, :new, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
