8f2581499ca7fbf377b5343c36f576a32e37be17
M.Nisar Ali <dr.nisaralig@gmail.com>
2023-10-04 19:30:09 +0500

    Define abilities for user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,18 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    #
-    #
+
+    user ||= User.new # guest user (not logged in)
+      if user.admin?
+        can :manage, :all
+        can :destroy, Post
+        can :destroy, Comment
+      else
+        can :destroy, Post, user_id: user.id
+        can :destroy, Comment, user_id: user.id
+        can :read, :all
+      end
+    
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
